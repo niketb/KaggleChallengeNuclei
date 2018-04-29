00906b33ff60f5dc7603d7b98412c152f637c88c
Anuja Tike <anujatike@verbsurgical.com>
2018-04-29 15:45:49 -0700

    Resizing all training images to size 256*256.

--- ResizeImages.py
+++ ResizeImages.py
@@ -1,7 +1,25 @@
 import os
-import keras
-import tensorflow
 from PIL import Image
 
 
-root="Users/anujatike/Documents/sem4/CS256/project/Data/stage1_train"
+
+def resizeImage(imageFilePath):
+    pass
+
+
+#Root path to search for "Images" folder in all image folders
+
+root='/Users/anujatike/Documents/sem4/CS256/project/Data/stage1_train'
+
+#Searching image to resize in Images folder
+for dirpath, dirs, files in os.walk(root):
+
+    if "images" in dirpath:
+
+        for filename in files:
+
+            imageFilePath=os.path.join(dirpath, filename)
+            resizeImage(imageFilePath)
+            print(imageFilePath)
+
+
